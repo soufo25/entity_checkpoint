80a8acbf4edbe22eafa927413f20d6d4a4a59897
soufo.25 <nteyouyousoufo>
2022-09-15 14:29:42 +0200

    entity_relation

---
+++ entitty.js
@@ -0,0 +1,47 @@
+
+// This is a JSON structure which represents the relationship existing 
+// between different elements of our data
+const gym = 
+{
+    "_id":1,
+    "name":"Gym of Hallad",
+    "address":"cairo",
+    "phoneNumber":0150-408-2054,
+    "session":{
+       "schedule":[8_10, 10_12, 12_2
+          
+       ],
+       "specialty":["Acrobatics", "yoga"
+          
+       ],
+       "instructor":[
+          {
+             "firstName":"Alex",
+             "lastName":"Burns",
+             "age":"26"
+          },
+          {
+             "firstName":"Nashi",
+             "lastName":"Sammy",
+             "age":"25"
+          }
+       ],"student":[
+          {
+             "firstName":"Lisa",
+             "lastName":"Nina",
+             "age":"20",
+             "phoneNumber":"0150-123-4054",
+             "gender":"female"
+          },
+          {
+             "firstName":"Modest",
+             "lastName":"Keylor",
+             "age":"21",
+             "phoneNumber":"0141-543-2531",
+             "gender":"male"
+          },
+          "n20"
+       ]
+    }
+ }
+
